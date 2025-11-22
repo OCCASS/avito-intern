66f5990395a954754bce20d1e94f642d6b90927b
Тимур Павлов <pavlov.timur556@yandex.ru>
2025-11-23 01:19:57 +0300

    feat: created pullrequest, user and team entities

---
+++ internal/entity/pull_request.go
@@ -0,0 +1,33 @@
+package entity
+
+import (
+	"fmt"
+	"slices"
+)
+
+type Status string
+
+const (
+	StatusOpen   Status = "OPEN"
+	StatusMerged Status = "MERGED"
+)
+
+var allowedStatuses = []Status{
+	StatusOpen,
+	StatusMerged,
+}
+
+func NewStatus(value string) (Status, error) {
+	s := Status(value)
+	if !slices.Contains(allowedStatuses, s) {
+		return "", fmt.Errorf("status %s is incorrect", value)
+	}
+	return s, nil
+}
+
+type PullRequest struct {
+	Id     int
+	Name   string
+	Author User
+	Status Status
+}
---
+++ internal/entity/team.go
@@ -0,0 +1,7 @@
+package entity
+
+type Team struct {
+	Id    int
+	Name  string
+	Users []User
+}
