4699dfc2639680ba05ec242377f4b24ccdae8289
Тимур Павлов <pavlov.timur556@yandex.ru>
2025-11-23 14:30:46 +0300

    feat: user repository created

---
+++ internal/domain/user/repository/postgres/postgres.go
@@ -0,0 +1,42 @@
+package postgres
+
+import (
+	"github.com/OCCASS/avito-intern/internal/database"
+	"github.com/OCCASS/avito-intern/internal/entity"
+)
+
+type UserPostgresRepository struct {
+	db *database.Database
+}
+
+func NewUserPostgresRepository(db *database.Database) *UserPostgresRepository {
+	return &UserPostgresRepository{db}
+}
+
+func (r UserPostgresRepository) UpdateIsActive(id string, newValue bool) (entity.User, error) {
+	var user entity.User
+	query := `UPDATE "user" SET is_active=$1 WHERE id=$2 RETURNING id, name, is_active`
+	if err := r.db.Conn.Get(&user, query, newValue, id); err != nil {
+		return entity.User{}, nil
+	}
+	return user, nil
+}
+
+func (r UserPostgresRepository) GetReview(id string) ([]entity.PullRequest, error) {
+	var prs []entity.PullRequest
+	query := `SELECT
+		pr.id,
+		pr.name,
+		pr.author_id,
+		pr.status,
+		pr.created_at,
+		pr.merged_at
+	FROM pullrequest_reviewer prr
+	JOIN pullrequest pr
+	ON pr.id = prr.pullrequest_id
+	WHERE prr.reviewer_id=$1`
+	if err := r.db.Conn.Select(&prs, query, id); err != nil {
+		return []entity.PullRequest{}, err
+	}
+	return prs, nil
+}
